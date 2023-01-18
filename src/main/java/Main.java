b0418a6d48eb5a3f4d06323c8cbbcbde61ad100e
Erika Rodriguez <rodriguez.eri92@gmail.com>
2023-01-18 15:14:38 -0300

    delete

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import mouredev.Challenge6;
+
+public class Main {
+    public static void main(String[] args) {
+//        fizzBuzz();
+//        System.out.println(isAnagram("saco","saco"));
+//        fibonacci();
+
+    }
+}
