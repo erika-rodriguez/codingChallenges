cbddbc59fb949ca1177b5dd3907dabe0456e7db0
Erika Rodriguez <rodriguez.eri92@gmail.com>
2023-01-18 14:46:07 -0300

    7th challenge from code signal

---
+++ src/main/java/codeSignal/AlmostIncreasingSequence.java
@@ -0,0 +1,37 @@
+package codeSignal;
+/*Codewriting
+Given a sequence of integers as an array, determine whether it is possible to obtain a
+ strictly increasing sequence by removing no more than one element from the array.
+Note: sequence a0, a1, ..., an is considered to be a strictly increasing if a0 < a1 < ... < an.
+Sequence containing only one element is also considered to be strictly increasing.
+
+Example
+For sequence = [1, 3, 2, 1], the output should be
+solution(sequence) = false.
+
+There is no one element in this array that can be removed in order to get a strictly increasing sequence.
+
+For sequence = [1, 3, 2], the output should be
+solution(sequence) = true.*/
+public class AlmostIncreasingSequence {
+
+    boolean solution(int [] sequence){
+        int count=0;
+
+        for (int i = 0; i < sequence.length-1; i++) {
+            if (sequence[i]>=sequence[i+1]){
+                count++;
+                if (i-1>=0 //limits the index to avoid IndexOutOfBounds
+                    &&i+2<=sequence.length-1
+                    &&sequence[i]>=sequence[i+2]
+                    &&sequence[i-1]>=sequence[i+1]) {
+                    return false;
+                }
+            }
+        }
+        if(count>1){
+            return false;
+        }
+        return true;
+    }
+}
