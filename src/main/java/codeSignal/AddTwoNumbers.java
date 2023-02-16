69c4dafa3a925bb8a9cc3fbfba26134617b17d32
Erika Rodriguez <rodriguez.eri92@gmail.com>
2023-02-16 14:49:06 -0300

    1st challenge from codeSignal

---
+++ src/main/java/codeSignal/AddTwoNumbers.java
@@ -0,0 +1,12 @@
+package codeSignal;
+/*1st challenge, level 1
+Write a function that returns the sum of two numbers.
+Example
+For param1 = 1 and param2 = 2, the output should be
+solution(param1, param2) = 3.*/
+public class AddTwoNumbers {
+    int solution(int param1, int param2) {
+        int sum;
+        return sum=param1+param2;
+    }
+}
