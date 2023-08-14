f15218aa414e09f5c45c19995ac66ecbe40be6d8
Erika Rodriguez <rodriguez.eri92@gmail.com>
2023-08-14 10:52:12 -0300

    20th challenge, maximal difference in int array

---
+++ src/main/java/codeSignal/ArrayMaximalAdjacentDifference.java
@@ -0,0 +1,27 @@
+package codeSignal;
+
+public class ArrayMaximalAdjacentDifference {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{2, 4, 1, 0}));
+    }
+
+    public static int solution(int[] numbers){
+        int difference=0;
+        for (int i = 0; i < numbers.length-1; i++) {
+            if (Math.abs(numbers[i]-numbers[i+1])>difference){
+                difference=Math.abs(numbers[i]-numbers[i+1]);
+            }
+        }
+        return difference;
+
+        /*OTHER SOLUTION
+        int solution(int[] inputArray) {
+        int max=0;
+        for( int i = 1 ; i < inputArray.length ; i++ ){
+        max = Math.max(max,Math.abs(inputArray[i] - inputArray[i-1]));
+        }
+        return max;
+        }
+         */
+    }
+}
